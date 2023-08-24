034e82a79a6c94913a65a77a1d2dbd2375dbf28f
Gokulasindhuja <136049113+Gokulasindhuja@users.noreply.github.com>
2023-08-24 21:01:42 +0530

    Update Html.jsx

--- src/pages/Html.jsx
+++ src/pages/Html.jsx
@@ -8,8 +8,12 @@ const Html = () => {
     <div className='lang'>
 <img src={img3} alt="" />
 <h3>HTML5 Markup</h3>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptates voluptas similique qui ipsa sunt aliquam? Quos minima voluptatibus illo, similique obcaecati blanditiis id eligendi saepe voluptates iusto soluta tempore quasi laudantium. Tempore, recusandae laboriosam dignissimos eaque illum aspernatur perspiciatis, deleniti obcaecati in dolorum at vel optio ratione est. Eius suscipit ab officia optio maiores! Vitae soluta accusamus animi quo consequatur ea repudiandae? Quis nisi iusto distinctio nihil repudiandae, illum voluptas labore, saepe consequuntur sint, nemo velit laudantium deleniti rerum ab at? Iure consectetur debitis illum totam, quam eaque ipsum quo esse at nulla suscipit soluta. Incidunt labore alias dignissimos.</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptates voluptas similique qui ipsa sunt aliquam? Quos minima voluptatibus illo, similique obcaecati blanditiis id eligendi saepe voluptates iusto soluta tempore quasi laudantium. Tempore, recusandae laboriosam dignissimos eaque illum aspernatur perspiciatis, deleniti obcaecati in dolorum at vel optio ratione est. Eius suscipit ab officia optio maiores! Vitae soluta accusamus animi quo consequatur ea repudiandae? Quis nisi iusto distinctio nihil repudiandae, illum voluptas labore, saepe consequuntur sint, nemo velit laudantium deleniti rerum ab at? Iure consectetur debitis illum totam, quam eaque ipsum quo esse at nulla suscipit soluta. Incidunt labore alias dignissimos.</p>
+<p>This course is part of the Web Design for Everybody: Basics of Web Development & Coding Specialization
+When you enroll in this course, you'll also be enrolled in this Specialization.</p>
+<p>Learn new concepts from industry experts</p>
+<p>Gain a foundational understanding of a subject or tool</p>
+<p>Develop job-relevant skills with hands-on projects</p>
+<p>Earn a shareable career certificate</p>
 <button onClick={() => navigate(-1)} className="buton">
             Go Back
           </button>

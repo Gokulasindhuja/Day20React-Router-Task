f10384e51c4e8f4faea1a5693f2fd7959634209f
Gokulasindhuja <136049113+Gokulasindhuja@users.noreply.github.com>
2023-08-24 21:06:46 +0530

    Update Html.jsx

--- src/pages/Html.jsx
+++ src/pages/Html.jsx
@@ -8,12 +8,9 @@ const Html = () => {
     <div className='lang'>
 <img src={img3} alt="" />
 <h3>HTML5 Markup</h3>
-<p>This course is part of the Web Design for Everybody: Basics of Web Development & Coding Specialization
-When you enroll in this course, you'll also be enrolled in this Specialization.</p>
-<p>Learn new concepts from industry experts</p>
-<p>Gain a foundational understanding of a subject or tool</p>
-<p>Develop job-relevant skills with hands-on projects</p>
-<p>Earn a shareable career certificate</p>
+<p>Welcome to HTML! HyperText Markup Language, or HTML, is the backbone of the web, providing the content, as well as the structure of that content, that you see displayed in your web browser.</p>
+<p>Unless you're reading a PDF or a printed version of this page, this content is made up of various HTML elements and text. HTML is the content layer of the web. HTML elements are the nodes that make up the Document Object Model.</p>
+<p>Cascading Style Sheets provide the look and feel, or presentation layer of the page. JavaScript is the behavior layer, often used to manipulate the objects within a document. Sites that are built with JavaScript frameworks are really just manipulating HTML. In turn, it's important to mark up your HTML in a way that scripts can easily parse and that assistive technologies can easily understand. This means writing HTML code with modern standards.</p>
 <button onClick={() => navigate(-1)} className="buton">
             Go Back
           </button>
